8afd2d19d8d90e7c604c35ae3e309d863fe98787
mirado <misie.koto@gmail.com>
2021-01-09 20:10:36 +0300

    Ajouter les fichiers

---
+++ src/Hook.php
@@ -0,0 +1,326 @@
+<?php
+
+namespace MiradoZk\WordPressHook;
+
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Logger;
+
+/**
+ * @author Mirado <miradozk@gmail.com>
+ */
+abstract class Hook
+{
+    /**
+     * Liste des hooks.
+     *
+     * @var array
+     */
+    protected $hooks = [];
+
+    /**
+     * identifiant du plugin.
+     *
+     * @var string
+     */
+    protected $plugin;
+
+    /**
+     * Fichier principal du plugin
+     *
+     * @var string
+     */
+    protected $filename;
+
+    /**
+     * Gestion des logs.
+     *
+     * @var \Monolog\Logger
+     */
+    protected $logger;
+
+    /**
+     * URL pour recuperer les informations sur le plugin.
+     *
+     * @var string|null
+     */
+    protected $hub = null;
+
+    /**
+     * Initialiser le plugin
+     *
+     * @param string $filename
+     * @param boolean $logger
+     */
+    public function __construct(string $filename, $logger = false)
+    {
+        $this->filename = $filename;
+        $this->plugin = plugin_basename($this->filename);
+
+        $this->resetHooks();
+
+        if ($logger) {
+            $this->setLogger();
+        }
+    }
+
+    /**
+     * Initialiser les hooks
+     *
+     * @return void
+     */
+    protected function resetHooks()
+    {
+        $this->hooks['actions'] = [];
+        $this->hooks['filters'] = [];
+        $this->hooks['shortcodes'] = [];
+        $this->hooks['install'] = [];
+        $this->hooks['uninstall'] = [];
+        $this->hooks['cli'] = [];
+    }
+
+    /**
+     * Definir le logger
+     *
+     * @return void
+     */
+    protected function setLogger()
+    {
+        $this->logger = new Logger($this->plugin);
+        $path = dirname($this->filename) . '/logs/' . $this->plugin . '.log';
+        $this->logger->pushHandler(new RotatingFileHandler($path, 4));
+    }
+
+    /**
+     * Récuperer l'instance du logger.
+     *
+     * @return \Monolog\Logger
+     */
+    public function getLogger()
+    {
+        return $this->logger;
+    }
+
+    /**
+     * Ajouter une action dans la registre.
+     *
+     * @param string   $hook
+     * @param callable $callback
+     * @param int      $args
+     *
+     * @return self
+     */
+    public function action($hook, $callback, $args = 0, $priority = 10): self
+    {
+        $action = [
+            'callback' => $callback,
+            'name' => $hook,
+            'priority' => $priority,
+        ];
+
+        if (!$args) {
+            $action['args'] = $args;
+        }
+
+        $this->hooks['actions'][] = $action;
+        
+        return $this;
+    }
+
+    /**
+     * Ajouter un filter.
+     *
+     * @param string   $hook
+     * @param callable $callback
+     * @param int      $args
+     *
+     * @return self
+     */
+    public function filter($hook, $callback, $args = 0, $priority = 10): self
+    {
+        $filter = [
+            'callback' => $callback,
+            'name' => $hook,
+            'priority' => $priority,
+        ];
+
+        if (!$args) {
+            $filter['args'] = $args;
+        }
+
+        $this->hooks['filters'][] = $filter;
+        
+        return $this;
+    }
+
+    /**
+     * Ajouter un shortcode.
+     *
+     * @param string   $name
+     * @param callable $callback
+     *
+     * @return self
+     */
+    public function shortcode($name, $callback) : self
+    {
+        $this->hooks['shortcodes'][] = [
+            'name' => $name,
+            'callback' => $callback,
+        ];
+
+        return $this;
+    }
+
+    /**
+     * Ajouter une action à la desactivation du plugin.
+     *
+     * @param callable $callback
+     *
+     * @return self
+     */
+    public function uninstall(callable $callback): self
+    {
+        $this->hooks['uninstall'][] = [
+            'name' => 'uninstall',
+            'callback' => $callback,
+        ];
+
+        return $this;
+    }
+
+    /**
+     * Ajouter une action à l'activation du plugin.
+     *
+     * @param callable $callback
+     *
+     * @return self
+     */
+    public function install(callable $callback): self
+    {
+        $this->hooks['install'][] = [
+            'name' => 'install',
+            'callback' => $callback,
+        ];
+
+        return $this;
+    }
+
+    /**
+     * Ajouter une commande WP-CLI.
+     *
+     * @param string   $commandname
+     * @param callable $callback
+     *
+     * @return self
+     */
+    public function command($commandname, $callback): self
+    {
+        $this->hooks['cli'][] = [
+            'callback' => $callback,
+            'name' => $commandname,
+        ];
+
+        return $this;
+    }
+
+    /**
+     * Déclarer les hooks.
+     */
+    abstract public function boot();
+
+    /**
+     * Executer les enregistrements des hooks.
+     *
+     * @return self
+     */
+    public function execute(): self
+    {
+        // Rendre updatable par defaut
+        $this->updatable();
+
+        // Enregistrer les hooks
+        $this->boot();
+
+        foreach ($this->hooks as $type => $hook) {
+            foreach ($hook as $items) {
+                switch ($type) {
+                    case 'actions':
+                        add_action($items['name'], $items['callback'], $items['priority'], $items['args']);
+                        break;
+
+                    case 'filters':
+                        add_filter($items['name'], $items['callback'], $items['priority'], $items['args']);
+                        break;
+
+                    case 'shortcodes':
+                        add_shortcode($items['name'], $items['callback']);
+                        break;
+
+                    case 'install':
+                        register_activation_hook($this->plugin, $items['callback']);
+                        break;
+
+                    case 'uninstall':
+                        register_deactivation_hook($this->plugin, $items['callback']);
+                        break;
+
+                    case 'cli':
+                        if (defined('WP_CLI') && WP_CLI) {
+                            \WP_CLI::add_command($items['name'], $items['callback']);
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Ajouter le gestionnaire de mise à jour.
+     *
+     * @return self
+     */
+    public function updatable(): self
+    {
+        $updater = new Updater($this->plugin, $this->hub, $this->logger());
+
+        $this->filter('site_transient_update_plugins', $updater, 1, 10);
+        $this->filter('plugins_api', [$updater, 'tabs'], 3, 20);
+        $this->action('upgrader_process_complete', [$updater, 'updated'], 2, 10);
+
+        return self;
+    }
+
+    /**
+     * Récuperer l'URL du plugin.
+     *
+     * @return string
+     */
+    public function url(): string
+    {
+        return plugin_dir_url($this->getFilename());
+    }
+
+    /**
+     * Récuperer le dossier du plugin.
+     *
+     * @return string
+     */
+    public function path(): string
+    {
+        return plugin_dir_path($this->getFilename());
+    }
+
+    /**
+     * Récuperer le filename du plugin (equivalent __FILE__).
+     *
+     * @return string
+     */
+    public function getFilename(): string
+    {
+        return $this->filename;
+    }
+}
---
+++ src/Updater.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace MiradoZk\WordPressHook;
+
+use GuzzleHttp\Client;
+use Throwable;
+
+/**
+ * Gestion des mises à jour
+ * 
+ * @author Mirado <miradozk@gmail.com>
+ */
+class Updater
+{
+    /**
+     * Informations concernant le plugin.
+     *
+     * @var \stdClass
+     */
+    private $plugin;
+
+    /**
+     * URL du hub.
+     *
+     * @var string|null
+     */
+    private $hub = null;
+
+    /**
+     * Client HTTP.
+     *
+     * @var GuzzleHttp\Client
+     */
+    private $http;
+
+    /**
+     * Intégrer l'updater nécessite le nom de l'application.
+     *
+     * @param string $name
+     */
+    public function __construct(string $name, string $hub)
+    {
+        $this->hub = $hub;
+        $this->plugin = new \stdClass();
+        $this->plugin->plugin = $name;
+        $this->plugin->slug = basename($name, '.php');
+        $this->plugin->cacheId = sprintf('update_%s', $this->plugin->slug);
+
+        $this->http = new Client([
+            'verify' => false,
+            'headers' => [
+                'Accept' => 'application/json',
+            ],
+        ]);
+    }
+
+    /**
+     * Télécharger la mise à jour.
+     *
+     * @param \stdClass $transient
+     *
+     * @return \stdClass
+     */
+    public function __invoke($transient)
+    {
+        if (empty($transient->checked)) {
+            return $transient;
+        }
+
+        $result = get_transient($this->plugin->cacheId);
+
+        if (false == $result) {
+            try {
+                $response = $this->http->request('GET', $this->hub . $this->plugin->slug);
+
+                $result = json_decode($response->getBody()->getContents());
+
+                if (!is_array($result->sections)) {
+                    $result->sections = (array) $result->sections;
+                }
+
+                if (!is_array($result->banners)) {
+                    $result->banners = (array) $result->banners;
+                }
+
+                set_transient($this->plugin->cacheId, $result, 360);
+            } catch (Throwable $e) {
+            }
+        }
+
+        if ($result) {
+            if (!function_exists('get_plugin_data')) {
+                require_once ABSPATH . 'wp-admin/includes/plugin.php';
+            }
+
+            $metadata = get_plugin_data(WP_PLUGIN_DIR . '/' . $this->plugin->plugin);
+
+            if ($result && -1 === version_compare($metadata['Version'], $result->Version)) {
+                $this->plugin->new_version = $result->Version;
+                $this->plugin->tested = $result->tested;
+                $this->plugin->package = $result->download_link;
+
+                $transient->response[$this->plugin->plugin] = $this->plugin;
+            }
+        }
+
+        return $transient;
+    }
+
+    /**
+     * Afficher les informations du plugins.
+     *
+     * @param stdClass $result
+     * @param string   $action
+     * @param stdClass $args
+     *
+     * @return mixed
+     */
+    public function tabs($result, $action, $args)
+    {
+        if ('plugin_information' != $action) {
+            return false;
+        }
+
+        if ($this->plugin->slug !== $args->slug) {
+            return $result;
+        }
+
+        $result = get_transient($this->plugin->cacheId);
+
+        if (!$result) {
+            try {
+                $response = $this->http->request('GET', $this->hub . $this->plugin->slug);
+
+                $result = json_decode($response->getBody()->getContents());
+
+                if (!is_array($result->sections)) {
+                    $result->sections = (array) $result->sections;
+                }
+
+                if (!is_array($result->banners)) {
+                    $result->banners = (array) $result->banners;
+                }
+
+                set_transient($this->plugin->cacheId, $result, 360);
+            } catch (Throwable $e) {
+                $result = false;
+            }
+        }
+
+        return $result ?: $transient;
+    }
+
+    /**
+     * Après mise à jour, nettoyez les caches.
+     *
+     * @param array $upgrader
+     * @param array $options
+     */
+    public function updated($upgrader, $options)
+    {
+        if ('update' == $options['action'] && 'plugin' == $options['type']) {
+            delete_transient($this->plugin->cacheId);
+        }
+    }
+}
